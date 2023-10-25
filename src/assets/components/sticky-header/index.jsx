67d4e29781714b21660e3895e08d79b3567978c1
Ferhat <ferhatkarakus.dev@gmail.com>
2023-10-25 18:32:16 +0300

    commit22

--- src/assets/components/sticky-header/index.jsx
+++ src/assets/components/sticky-header/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const StickyHeader = () => {
   return (
-    <header className="sticky top-0 z-10 bg-black/[.65] backdrop-blur-md">
+    <header className="sticky top-0 z-10 bg-black/[.65] backdrop-blur-md sticky">
       <h3 className="px-4 h-[3.3rem] flex items-center text-xl border-b border-[#2f3336]">
         Home{" "}
       </h3>
